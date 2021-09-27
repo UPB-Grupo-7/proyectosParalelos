b9a4e76d95d16a18a543f65095a7e451cff8884c
OneMoreLady <81486565+OneMoreLady@users.noreply.github.com>
2021-09-27 09:52:22 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+const btnToggle = document.querySelector('.toggle-btn');
+btnToggle.addEventListener('click', function() {
+    document.getElementById('landing_navbar').classList.toggle('active');
+});

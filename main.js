799b77907339f7a7264f511d3b6d28756dda68ff
OneMoreLady <81486565+OneMoreLady@users.noreply.github.com>
2021-09-27 09:56:05 -0500

    Delete main.js

--- main.js
+++
@@ -1,4 +0,0 @@
-const btnToggle = document.querySelector('.toggle-btn');
-btnToggle.addEventListener('click', function() {
-    document.getElementById('landing_navbar').classList.toggle('active');
-});
